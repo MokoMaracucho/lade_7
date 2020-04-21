5592e7519effd215a7fdf9bba573836e75af0d7d
MokoMaracucho <desmoulin.federico@gmail.com>
2020-04-21 19:21:00 +0100

    Service : UtilisateurServiceImpl.java

---
+++ src/main/java/com/oc/moko/lade/service/UtilisateurServiceImpl.java
@@ -0,0 +1,41 @@
+package com.oc.moko.lade.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.oc.moko.lade.entity.Utilisateur;
+
+@Service
+public class UtilisateurServiceImpl implements UtilisateurService {
+
+    @Autowired
+    private UtilisateurDao utilisateurDao;
+
+	@Override
+    @Transactional
+	public void enregistrerUtilisateur(Utilisateur utilisateur) {
+		utilisateurDao.enregistrerUtilisateur(utilisateur);
+	}
+
+	@Override
+    @Transactional
+	public Utilisateur selectionUtilisateurParId(UUID idUtilisateur) {
+        return utilisateurDao.selectionUtilisateurParId(idUtilisateur);
+	}
+
+	@Override
+    @Transactional
+	public List<Utilisateur> listeUtilisateurs() {
+        return utilisateurDao.listeUtilisateurs();
+	}
+
+	@Override
+    @Transactional
+	public void supprimerUtilisateurParId(UUID idUtilisateur) {
+		utilisateurDao.supprimerUtilisateurParId(idUtilisateur);
+	}
+}
