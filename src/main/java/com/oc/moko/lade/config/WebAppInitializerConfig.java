c30c3d34e8012ad6e2878a41b3fcaf06d2300b3a
MokoMaracucho <desmoulin.federico@gmail.com>
2020-04-22 11:13:50 +0100

    Config : WebAppInitializerConfig.java

---
+++ src/main/java/com/oc/moko/lade/config/WebAppInitializerConfig.java
@@ -0,0 +1,27 @@
+package com.oc.moko.lade.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializerConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {
+            PersistenceJPAConfig.class
+        };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {
+            WebMvcConfig.class
+        };
+	}
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {
+            "/"
+        };
+    }
+}
