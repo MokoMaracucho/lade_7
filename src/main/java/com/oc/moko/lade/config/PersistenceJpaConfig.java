3677b68f3e4b5d46a7a9748c8aa47d7cee8e663a
MokoMaracucho <desmoulin.federico@gmail.com>
2020-04-22 16:00:27 +0100

    Chargement application : OK·

--- src/main/java/com/oc/moko/lade/config/PersistenceJpaConfig.java
+++ src/main/java/com/oc/moko/lade/config/PersistenceJpaConfig.java
@@ -22,7 +22,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-@PropertySource({"classpath:database.properties"})
+@PropertySource({"classpath:/com/oc/moko/lade/config/database.properties"})
 @ComponentScan({"com.oc.moko.lade"})
 @EnableJpaRepositories(basePackages="com.oc.moko.lade.repository")
 public class PersistenceJpaConfig {
